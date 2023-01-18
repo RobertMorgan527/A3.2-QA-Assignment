5695ba61a7bbd8ad8f43252ef61ba91c9a04c192
Robert <Robert.Morgan@Exclaimer.com>
2023-01-18 17:51:49 +0000

    Add project files.

---
+++ QAExceptions.cs
@@ -0,0 +1,20 @@
+﻿public class LoginException : Exception
+{
+    public LoginException(String message) : base(message)
+    {
+    }
+}
+public class InvalidUKAddressException : Exception
+{
+    public InvalidUKAddressException(String message) : base(message)
+    {
+    }
+}
+
+public class TooManyLoginTriesException : Exception
+{
+    public TooManyLoginTriesException() : base("Login failed more than 3 times")
+    {
+    }
+}
+
